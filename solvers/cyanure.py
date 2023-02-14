012e95bcea9fa6dd7ac34e5a32c69ac32bca4b07
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-02-14 16:24:51 +0100

    fix: name

--- solvers/cyanure.py
+++ solvers/cyanure.py
@@ -11,7 +11,7 @@ with safe_import_context() as import_ctx:
 
 
 class Solver(BaseSolver):
-    name = 'cyanure_norm'
+    name = 'cyanure'
 
     install_cmd = 'conda'
     requirements = ['cyanure']
